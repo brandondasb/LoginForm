8d4d89e277d901919f25567755060ec93adf0bd9
brandonmilambo <brandonmilambo.dev@gmail.com>
2019-07-21 07:04:56 +0100

    homefragment: added spinner to choose what seasaon to display

--- app/src/main/java/com/dasb/brandonmilambo/loginform/view/HomeFragment.java
+++ app/src/main/java/com/dasb/brandonmilambo/loginform/view/HomeFragment.java
@@ -7,6 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.dasb.brandonmilambo.loginform.R;
 import com.dasb.brandonmilambo.loginform.api.FootballDataApi;
@@ -17,12 +21,15 @@ import com.dasb.brandonmilambo.loginform.model.GsonStandingsResponse;
 import com.dasb.brandonmilambo.loginform.presenter.HomeStandingPresenter;
 import com.dasb.brandonmilambo.loginform.repo.FootballDataRepo;
 
-public class HomeFragment extends BaseNavFragment {
+public class HomeFragment extends BaseNavFragment implements AdapterView.OnItemSelectedListener {
     private HomeStandingPresenter homeStandingPresenter;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
         return inflater.inflate(R.layout.fragment_home, container, false);
 
     }
@@ -36,7 +43,18 @@ public class HomeFragment extends BaseNavFragment {
 
 
         FootballDataRepo footballDataRepo = new FootballDataRepo();
-        footballDataRepo.getStandingFordId("2021", "2018" ,standingCallback);
+        footballDataRepo.getStandingFordId("2021", "2018", standingCallback);
+        Toast.makeText(this.getActivity(), "last season test", Toast.LENGTH_LONG);
+
+
+        ///spinner implementation
+        Spinner spinner = getView().findViewById(R.id.spinnerSeason);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.season, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 
     private StandingCallback standingCallback = new StandingCallback() {
@@ -46,4 +64,30 @@ public class HomeFragment extends BaseNavFragment {
             homeStandingPresenter.Load(response.getStandings().get(0).getTable());
         }
     };
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        FootballDataRepo footballDataRepo = new FootballDataRepo();
+
+        /////
+        switch (position) {
+            case 0:
+                footballDataRepo.getStandingFordId("2021", "2019", standingCallback);
+                Toast.makeText(this.getContext(), "season 2019/20", Toast.LENGTH_LONG).show();
+                break;
+            case 1:
+                footballDataRepo.getStandingFordId("2021", "2018", standingCallback);
+                Toast.makeText(parent.getContext(), "season 2018/19", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        Toast.makeText(parent.getContext(), "nothing selected", Toast.LENGTH_LONG).show();
+
+
+    }
 }
